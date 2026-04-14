f334fde55f56ade2c8fd441fc69fe5365246036a
atihlev-collab <atihlev@gmail.com>
2026-04-14 20:11:53 +0300

    Update ultimate_plus_bot_v7_vip.py

--- ultimate_plus_bot_v7_vip.py
+++ ultimate_plus_bot_v7_vip.py
@@ -125,7 +125,7 @@ def ai_live(m, stats):
         score, pick = 85, "UNDER 2.5"
 
     # FILTER
-    if score < (88 if MODE == "SAFE" else 78):
+    if score < (80 if MODE == "SAFE" else 75):
         return None
 
     return score, pick
